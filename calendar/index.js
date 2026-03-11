0932b3cf577579a61369d724c3dfa076853ab12a
VSKat666 <vskarpova2010@gmail.com>
2026-03-11 16:41:04 +0500

    Update index.js

--- calendar/index.js
+++ calendar/index.js
@@ -409,7 +409,7 @@ const tracks = [
     "https://track.pinkamuz.pro/download/33313731b1b43436348d37313533373331b4300300/6a4cbf244c1b56414611758459673940/%D0%9D%D0%B5%D1%80%D0%B2%D1%8B%20-%20%D0%9F%D0%BE%D0%B4%D0%BE%D0%BA%D0%BE%D0%BD%D0%BD%D0%B8%D0%BA.mp3", //27
     "https://track.pinkamuz.pro/download/33313731b1b434b2308937313533b3303630350300/d1e668e98a691a11d3c2dea9e1668a67/Dabro%20-%20%D0%94%D0%B0%D0%B2%D0%B0%D0%B9%20%D0%B7%D0%B0%D0%BF%D0%BE%D1%91%D0%BC.mp3", //28
     "https://ruo.morsmusic.org/load/1968177103/Artur_Pirozhkov_-_Samo_Sobojj_(musmore.org).mp3", //29
-    "https://track.pinkamuz.pro/download/3336373437313531b18c37313533b2343035340300/7003eef4de28cd159f6794fe94197dcc/%D0%94%D0%B8%D1%81%D0%BA%D0%BE%D1%82%D0%B5%D0%BA%D0%B0%20%D0%90%D0%B2%D0%B0%D1%80%D0%B8%D1%8F%20-%20%D0%9D%D0%BE%D0%B2%D0%BE%D0%B3%D0%BE%D0%B4%D0%BD%D1%8F%D1%8F.mp3" //30
+    "https://track.pinkamuz.pro/download/3336373437313531b18c37313533b2343035340300/7003eef4de28cd159f6794fe94197dcc/%D0%94%D0%B8%D1%81%D0%BA%D0%BE%D1%82%D0%B5%D0%BA%D0%B0%20%D0%90%D0%B2%D0%B0%D1%80%D0%B8%D1%8F%20-%20%D0%9D%D0%BE%D0%B2%D0%BE%D0%B3%D0%BE%D0%B4%D0%BD%D1%8F%D1%8F.mp3", //30
     "https://ru-d2.drivemusic.me/dl/-534byk41Hf-FyrXaA_JRg/1773207010/download_music/2025/12/ruki-vverkh-novyjj-god.mp3"
     ];
 
@@ -571,6 +571,7 @@ const tracks = [
                 nextTrack();
             });
         }        
+
 
 
 
